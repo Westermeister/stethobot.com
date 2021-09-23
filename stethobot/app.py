28018bc209cc2bf057ea84c606881c0499e36381
Westermeister <westermeister12@gmail.com>
2021-09-22 18:55:16 -0700

    Implement rest of flask app and update templates

--- stethobot/app.py
+++ stethobot/app.py
@@ -1,6 +1,10 @@
+# Copyright (c) 2021 The Stethobot Authors. All rights reserved.
+
 """Flask entry point."""
 
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, request, send_file
+
+from api.model import DiagnosticModel
 
 app = Flask(__name__)
 
@@ -35,39 +39,106 @@ def conditions():
     return render_template("conditions/index.html")
 
 
-@app.route("/conditions/breast-cancer")
+@app.route("/conditions/breast-cancer", methods=["GET", "POST"])
 def breast_cancer():
-    """Render the breast cancer input form."""
-    return render_template("conditions/breast-cancer.html")
+    """Handle diagnosis requests for breast cancer."""
+    if request.method == "GET":
+        return render_template("conditions/breast-cancer.html")
+    else:
+        ORDER = [
+            "radius",
+            "texture",
+            "perimeter",
+            "area",
+            "smoothness",
+            "compactness",
+            "concavity",
+            "concave",
+            "symmetry",
+            "fractal",
+        ]
+        observation = []
+        for i in range(3):
+            for name in ORDER:
+                csv = request.form.get(name).split(",")
+                observation.append(float(csv[i]))
+        model = DiagnosticModel()
+        with open("./api/breast_cancer/model.json") as f:
+            model.from_json(f.read())
+        diagnosis, confidence_score = model.diagnose(observation)
+        return render_template(
+            "conditions/diagnosis.html",
+            condition="breast cancer",
+            diagnosis=diagnosis,
+            confidence_score=confidence_score,
+        )
 
 
-@app.route("/conditions/diabetes")
+@app.route("/conditions/diabetes", methods=["GET", "POST"])
 def diabetes():
-    """Render the diabetes input form."""
-    return render_template("conditions/diabetes.html")
+    """Handle diagnosis requests for diabetes."""
+    if request.method == "GET":
+        return render_template("conditions/diabetes.html")
+    else:
+        ORDER = [
+            "pregnancies",
+            "plasma",
+            "diastolic",
+            "skin",
+            "insulin",
+            "bmi",
+            "pedigree",
+            "age",
+        ]
+        observation = []
+        for name in ORDER:
+            observation.append(float(request.form.get(name)))
+        model = DiagnosticModel()
+        with open("./api/diabetes/model.json") as f:
+            model.from_json(f.read())
+        diagnosis, confidence_score = model.diagnose(observation)
+        return render_template(
+            "conditions/diagnosis.html",
+            condition="diabetes",
+            diagnosis=diagnosis,
+            confidence_score=confidence_score,
+        )
 
 
-@app.route("/conditions/heart-disease")
+@app.route("/conditions/heart-disease", methods=["GET", "POST"])
 def heart_disease():
-    """Render the heart disease input form."""
-    return render_template("conditions/heart-disease.html")
-
-
-@app.route("/api/diagnose/breast-cancer")
-def breast_cancer_diagnosis():
-    """Return the diagnosis for breast cancer."""
-    return  # TODO
-
-
-@app.route("/api/diagnosis/diabetes")
-def diabetes_diagnosis():
-    """Return the diagnosis for diabetes."""
-    return  # TODO
-
-
-@app.route("/api/diagnosis/heart-disease")
-def heart_disease_diagnosis():
-    return  # TODO
+    """Handle diagnosis requests for heart disease."""
+    if request.method == "GET":
+        return render_template("conditions/heart-disease.html")
+    else:
+        ORDER = [
+            "age",
+            "sex",
+            "chest",
+            "pressure",
+            "serum",
+            "sugar",
+            "elec",
+            "thalach",
+            "exang",
+            "oldpeak",
+            "slope",
+            "vessels",
+            "thal",
+        ]
+        observation = []
+        for name in ORDER:
+            observation.append(float(request.form.get(name)))
+        model = DiagnosticModel()
+        with open("./api/heart_disease/model.json") as f:
+            model.from_json(f.read())
+        diagnosis, confidence_score = model.diagnose(observation)
+        return render_template(
+            "conditions/diagnosis.html",
+            condition="heart disease",
+            diagnosis=diagnosis,
+            confidence_score=confidence_score,
+        )
 
 
 @app.route("/docs/breast-cancer")
